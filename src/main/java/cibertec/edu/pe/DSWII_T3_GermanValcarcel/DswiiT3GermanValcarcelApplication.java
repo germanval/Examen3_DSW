bab3ff33f9c8a01dff4c60c4a959a9b7dadca377
germanval <germanvalcarcel_1500@hotmail.com>
2024-07-02 14:10:05 -0500

    pregunta 1 inicio

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_GermanValcarcel/DswiiT3GermanValcarcelApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_GermanValcarcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3GermanValcarcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3GermanValcarcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_T3_GermanValcarcel/DswiiT3GermanValcarcelApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_GermanValcarcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3GermanValcarcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
